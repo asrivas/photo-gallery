4a5824985f323c52bdbded5703ef9b45ef0e2ad1
asrivas <asrivastava274@gmail.com>
2013-09-24 07:14:32 -0700

    added favicon

--- create_album.php
+++ create_album.php
@@ -73,7 +73,7 @@ $success = isset($_POST['AddAlbum']) && ($_POST['title'] != "") && $uploaded;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="shortcut icon" href="bootstrap-3.0.0/assets/ico/favicon.png">
+    <link rel="shortcut icon" href="favicon.ico">
 
     <title>Instant Photo Gallery</title>
 
