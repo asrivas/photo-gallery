be7a02824ab0370300eb328cd755980a74118612
asrivas <asrivastava274@gmail.com>
2013-09-23 20:54:03 -0700

    added photos and starting create album form

--- create_album.php
+++ create_album.php
@@ -1,3 +1,17 @@
 <?php 
-
+	$success = isset($_POST['AddAlbum']) && ($_POST['title'] != "");
 ?>
+
+<?php
+	if($success){
+		echo 'Congrats, the album was created';
+	}
+?>
+
+<form action="signup.php" method="post" enctype="multipart/form-data">
+	<fieldset>
+		<label>Album Title: 
+		<input type="text" name="title" />
+		</label>
+	</fieldset>
+</form>
